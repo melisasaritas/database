0d0db77d80e4ba750f3d9244ad2281ec06204186
sf0831 <bluesky6653@hotmail.com>
2024-05-31 16:54:10 +0300

    latest version, imported views!!

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import custom_register_view, custom_view, custom_login_view, custom_home_view, add_to_cart, view_cart, checkout, remove_item, custom_computers_view
+from .views import custom_register_view, custom_view, custom_login_view, custom_home_view, add_to_cart, view_cart, checkout, remove_item, custom_computers_view, custom_stock_data, custom_components_view
 
 app_name = 'myapp'
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/<int:ram>/<str:price>/<int:processor>/<str:name>/',
          add_to_cart, name='add_to_cart'),
 
-    path('BuildPc', TemplateView.as_view(template_name="shop.html")),
+    path('BuildPc', custom_components_view, name="shop.html"),
     path('PreMade', custom_computers_view, name="computers.html"),
 
     path('ContactUs', TemplateView.as_view(template_name="contact.html")),
@@ -20,8 +20,7 @@ urlpatterns = [
 
     path('AdminPanel/', TemplateView.as_view(template_name="index2.html")),
     path('AdminPanel/History', TemplateView.as_view(template_name="history.html")),
-    path('AdminPanel/Stock_Data',
-         TemplateView.as_view(template_name="stock_data.html")),
+    path('AdminPanel/Stock_Data',custom_stock_data, name= "stock_data.html"),
     path('AdminPanel/User_Data',
          TemplateView.as_view(template_name="user_data.html")),
 
